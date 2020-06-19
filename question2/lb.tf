a1357f3194963b26b748601cb99d85a4895d3ed6
mirka <mirkarimov@gmail.com>
2020-06-18 20:53:19 -0700

    added db lb

--- question2/lb.tf
+++ question2/lb.tf
@@ -30,3 +30,11 @@ resource "azurerm_lb" "esodahealth-tomcat" {
   resource_group_name = "${var.rg_name_prefix}-${each.value}"
 
 }
+
+resource "azurerm_lb" "esodahealth-db" {
+  for_each            = toset(var.locations)
+  name                = "lb-db-${each.value}"
+  location            = "${each.value}"
+  resource_group_name = "${var.rg_name_prefix}-${each.value}"
+
+}
